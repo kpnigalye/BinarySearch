c28457c6d64cce96c7fd6b98e5ab4f7c1334a192
Krishna Nigalye <krishna.nigalye@gmail.com>
2023-09-25 12:19:13 +0530

    Create SearchRange.java

---
+++ SearchRange.java
@@ -0,0 +1,40 @@
+package Tutorial;
+
+import java.util.Arrays;
+
+public class BSFirstAndLastOccurance {
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        System.out.println(Arrays.toString(searchRange(nums, target)));
+    }
+
+    public static int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findOccuranceOfElement(nums, target, true);
+        result[1] = findOccuranceOfElement(nums, target, false);
+        return result;
+    }
+
+    static int findOccuranceOfElement(int[] nums, int target, boolean findFirstIndex) {
+        int start = 0, end= nums.length-1;
+        int index = -1;
+
+        while (start <= end) {
+            int med = start + (end-start)/2;
+
+            if(target < nums[med]) {
+                end = med-1;
+            } else if (target > nums[med]) {
+                start = med+1;
+            } else {
+                index = med;
+                if(findFirstIndex)
+                    end = med-1;
+                else
+                    start = med+1;
+            }
+        }
+        return index;
+    }
+}
